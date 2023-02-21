11bcd69466d4b59c501bc2e79327a8fa5aab82a4
igorBekin <igor_bekin@mail.ru>
2023-02-21 13:30:51 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Игорь Бекин on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
